a66ea3d0567f04a6e05e66bacd8f8ad4fa0092f6
estebanc7856 <cte2026256@est.univalle.edu>
2025-10-22 11:17:37 -0400

    Eliminar archivos antiguos del repositorio

--- backend/src/routes/usuarios.js
+++
@@ -1,9 +0,0 @@
-// src/routes/usuarios.js
-const express = require('express');
-const router = express.Router();
-const { cambiarPasswordPrimeraVez } = require('../controllers/authController');
-const { verificarToken } = require('../middleware/auth'); // middleware JWT
-
-router.post('/cambiar-password-primera-vez', verificarToken, cambiarPasswordPrimeraVez);
-
-module.exports = router;
--- backend/src/services/generarContrasena.js
+++
@@ -1,7 +0,0 @@
-function generarContrasena(longitud = 12) {
-    const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+';
-    return Array.from({ length: longitud }, () => caracteres.charAt(Math.floor(Math.random() * caracteres.length))).join('');
-  }
-  
-  module.exports = generarContrasena;
-  
