9a75043e17b583dd587d2b26bd7552a084820670
estebanc7856 <cte2026256@est.univalle.edu>
2025-12-07 13:12:34 -0400

    api prediagnosticos corregido2

--- backend/src/routes/prediagnosticos.js
+++ backend/src/routes/prediagnosticos.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const {
   crearPrediagnosticoController,
-} = require('../controllers/prediagnostico.controller');
+} = require('../controllers/prediagnostico');
 
 // POST /api/prediagnosticos
 router.post('/prediagnosticos', crearPrediagnosticoController);
